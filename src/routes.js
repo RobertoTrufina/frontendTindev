70428afb85392907a6485dc4a52bf38b841a269e
Roberto Pinto <robertotrufina@gmail.com>
2022-05-01 11:37:33 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default function ProjectRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={ <Login /> } />
+                <Route path="/main" element={ <Main /> } />
+            </Routes>
+        </BrowserRouter>           
+    );
+}
