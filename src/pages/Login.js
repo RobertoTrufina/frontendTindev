2408a2c339850a92703b783f61b7ca3fa1d8df2d
Roberto Pinto <robertotrufina@gmail.com>
2022-05-01 14:23:40 -0300

    Atualização do Main e Login mais

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,38 +1,37 @@
-import React, { useState } from "react";
-import logo from '../assets/logo.svg';
-import api from "../services/api";
-import './Login.css';
+import React, { useState } from 'react'
+import logo from '../assets/logo.svg'
+import api from '../services/api.js'
+import './Login.css'
 
 
 
-export default function Login({ history }) {
-    const [username, setUsername] = useState('');
+export default function Login( { history }) {
+    const [username, setUsername] = useState('')
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+    async function handleSubmit(event) {
+        event.preventDefault()
 
         const response = await api.post('/devs', {
-            username,
+            username: username
         })
 
-        console.log(response);
+        const { _id } = response.data
 
-        history.pushState('/main');
-
-    } 
+        history.push(`/dev/${_id}`)
+    }
 
     return (
         <div className="login-container">
             <form onSubmit={handleSubmit}>
-                <img src={logo} alt="Tindev" />
-                <input 
-                placeholder="Digite seu usuário no Github" 
-                value={username} 
-                onChange={e => setUsername(e.target.value)}
+                <img src={logo} alt="Tindev Logo"/>
+                <input
+                    placeholder="Digite seu usuário no Github"
+                    value={ username }
+                    onChange={ event => setUsername(event.target.value) }
                 />
                 <button type="submit">Enviar</button>
+                
             </form>
         </div>
-         
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import Login from './pages/Login';
 import Main from './pages/Main';
+
 
 export default function ProjectRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact element={ <Login /> } />
-                <Route path="/main" element={ <Main /> } />
+                <Route path="/dev/:id" element={ <Main /> } />
             </Routes>
         </BrowserRouter>           
     );
